bfa472a2caa353c10e802a6790d897fae56a279a
gibney <andyclark2020@gmail.com>
2024-02-24 17:46:32 +0100

    Added 15 houses to the site

---
+++ site/snippets/layouts/table.php
@@ -0,0 +1,32 @@
+<?php
+/*
+    Multi line comment
+    <thead>
+    <tr>
+      <th>Song</th>
+      <th>Artist</th>
+      <th>Year</th>
+    </tr>
+  </thead>
+*/
+?>
+<table class="table-auto">
+<caption> Heading of table</caption>
+  <tbody>
+    <tr>
+      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+      <td>Malcolm Lockyer</td>
+      <td>1961</td>
+    </tr>
+    <tr>
+      <td>Witchy Woman</td>
+      <td>The Eagles</td>
+      <td>1972</td>
+    </tr>
+    <tr>
+      <td>Shining Star</td>
+      <td>Earth, Wind, and Fire</td>
+      <td>1975</td>
+    </tr>
+  </tbody>
+</table>
